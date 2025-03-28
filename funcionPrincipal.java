d82457f735e06a5b917a925e9ce0459d2d3bfc94
Cristian Yavhe <yavhecristian05@gmail.com>
2025-03-28 09:24:40 -0600

    Co-authored-by: fco-j-t-g <fco-j-t-g@users.noreply.github.com> Co-authored-by: Sixzeng352 <Sixzeng352@users.noreply.github.com>

--- funcionPrincipal.java
+++ funcionPrincipal.java
@@ -1,5 +1,7 @@
 public class funcionPrincipal {
    public static void main (String [] args){
-
+      Formularios f1=new Formularios();
+        f1.ventanaGeneral();
+        f1.cuentaInstitucional();
    }//Fin main
 }//Fin class examen
